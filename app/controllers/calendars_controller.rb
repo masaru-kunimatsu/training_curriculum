e6b3e983e6be9eca490b870f2560c94511ff07e9
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-06-26 06:16:33 +0900

    Issue2
    11行目と17行目の記述をplan_からPlanに変更しました。

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,13 +8,13 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(plan_params)
+    Plan.create(Plan_params)
     redirect_to action: :index
   end
 
   private
 
-  def plan_params
+  def Plan_params
     params.require(:calendars).permit(:date, :plan)
   end
 
