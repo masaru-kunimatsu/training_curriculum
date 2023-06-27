01afc4301606d7279536cd04faae6899ff757920
masaru-kunimatsu <kmikyakbikfq06i@yahoo.co.jp>
2023-06-28 05:32:10 +0900

    Issue1〜3の2回目の修正
    頂いたコメントをもとに再度修正しました。ご確認お願いします。

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,13 +8,13 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(Plan_params)
+    Plan.create(plan_params)
     redirect_to action: :index
   end
 
   private
 
-  def Plan_params
+  def plan_params
     params.require(:calendars).permit(:date, :plan)
   end
 
